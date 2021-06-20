add724703479e705dda3d750a091325ffd9a40e8
Ivan Matveev <vanadiiii42@gmail.com>
2021-06-20 10:46:41 +0400

    [1] check AddressMapper

---
+++ src/test/java/ru/dexsys/mapstructdemo/mapper/AddressMapperTest.java
@@ -0,0 +1,40 @@
+package ru.dexsys.mapstructdemo.mapper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.mockito.Spy;
+import ru.dexsys.mapstructdemo.dto.AddressDto;
+import ru.dexsys.mapstructdemo.entity.Address;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+class AddressMapperTest {
+    @Spy
+    private final AddressMapper addressMapper = Mappers.getMapper(AddressMapper.class);
+
+    private AddressDto addressDto;
+
+    @BeforeEach
+    void initAddressDto() {
+        addressDto = new AddressDto();
+        addressDto.setCity("Izhevsk");
+        addressDto.setStreet("Udmurtskaya");
+        addressDto.setHouseNumber("304");
+    }
+
+    @Test
+    void applyTest() {
+        Address address = addressMapper.apply(addressDto);
+
+        log.info(addressDto.toString());
+        log.info(address.toString());
+
+        assertEquals(addressDto.getCity(), address.getCity());
+        assertEquals(addressDto.getStreet(), address.getStreet());
+        assertEquals(addressDto.getHouseNumber(), address.getHouseNumber());
+
+    }
+}
