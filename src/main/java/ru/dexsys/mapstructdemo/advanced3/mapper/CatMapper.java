5c413736ae65f6017953ee369bbe102454b65361
vanadiiii <vanadiiii42@gmail.com>
2021-07-07 00:11:43 +0400

    [12] mapping inheritance strategy #2

--- src/main/java/ru/dexsys/mapstructdemo/advanced3/mapper/CatMapper.java
+++ src/main/java/ru/dexsys/mapstructdemo/advanced3/mapper/CatMapper.java
@@ -3,10 +3,12 @@ package ru.dexsys.mapstructdemo.advanced3.mapper;
 import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingInheritanceStrategy;
 import ru.dexsys.mapstructdemo.advanced3.dto.CatDto;
 import ru.dexsys.mapstructdemo.advanced3.entity.Cat;
 
-@Mapper(config = MammalAnimalMapperConfig.class)
+@Mapper(config = MammalAnimalMapperConfig.class,
+        mappingInheritanceStrategy = MappingInheritanceStrategy.AUTO_INHERIT_ALL_FROM_CONFIG)
 public interface CatMapper {
 
     @InheritConfiguration(name = "mapMammal")
--- src/main/java/ru/dexsys/mapstructdemo/advanced3/mapper/MammalAnimalMapperConfig.java
+++ src/main/java/ru/dexsys/mapstructdemo/advanced3/mapper/MammalAnimalMapperConfig.java
@@ -3,10 +3,12 @@ package ru.dexsys.mapstructdemo.advanced3.mapper;
 import org.mapstruct.InheritConfiguration;
 import org.mapstruct.MapperConfig;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingInheritanceStrategy;
 import ru.dexsys.mapstructdemo.advanced3.dto.MammalAnimalDto;
 import ru.dexsys.mapstructdemo.advanced3.entity.MammalAnimal;
 
-@MapperConfig(componentModel = "spring")
+@MapperConfig(componentModel = "spring",
+        mappingInheritanceStrategy = MappingInheritanceStrategy.AUTO_INHERIT_ALL_FROM_CONFIG)
 public interface MammalAnimalMapperConfig extends AnimalMapperConfig {
 
     @InheritConfiguration
